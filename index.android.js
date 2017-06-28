ca625e50f2ab10dccb8bb23b546891dde22c215b
GarethMc <gmcdonnell@cartrwarler.com>
2017-06-28 16:03:29 +0100

    first commit

---
+++ index.android.js
@@ -0,0 +1,59 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity
+} from 'react-native';
+
+import codePush from 'react-native-code-push';
+
+export default class GreenhouseExample extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to an iOS App with CI!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+const codePushOptions = { checkFrequency: codePush.CheckFrequency.ON_APP_RESUME};
+const AppWithCodePush = codePush(codePushOptions)(GreenhouseExample);
+
+AppRegistry.registerComponent('ok', () => AppWithCodePush);
